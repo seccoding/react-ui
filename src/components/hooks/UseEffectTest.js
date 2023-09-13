981c9f077b2296db16c41830145a07dfd9eb26db
Min Chang Jang <mcjang1116@gmail.com>
2023-09-13 15:33:00 +0900

    UseEffect

---
+++ src/components/hooks/UseEffectTest.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export default function EffectComponent() {
+
+    const [memberData, setMemberData] = useState([])
+    const [name, setName] = useState("")
+    const [age, setAge] = useState("")
+    const [address, setAddress] = useState("")
+    
+
+}
