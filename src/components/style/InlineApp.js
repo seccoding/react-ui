7e39689836a4ddb2e94369ed004c98635c79b1eb
Min Chang Jang <mcjang1116@gmail.com>
2023-09-13 13:24:11 +0900

    InlineApp

---
+++ src/components/style/InlineApp.js
@@ -0,0 +1,15 @@
+import InlineBox from "./InlineBox";
+
+export default function InlineApp() {
+
+    return (
+        <div>
+            <InlineBox letter="A" bgColor="#EEE" />
+            <InlineBox letter="E" bgColor="#CCC" />
+            <InlineBox letter="I" bgColor="#AAA" />
+            <InlineBox letter="O" bgColor="#999" />
+            <InlineBox letter="U" bgColor="#888" />
+        </div>
+    );
+
+}
