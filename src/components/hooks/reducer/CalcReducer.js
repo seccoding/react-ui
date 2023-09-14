458bd9767833eadae797a7048f13b296cb02955b
Min Chang Jang <mcjang1116@gmail.com>
2023-09-14 09:18:21 +0900

    useReducer

---
+++ src/components/hooks/reducer/CalcReducer.js
@@ -0,0 +1,8 @@
+// const [money, dispatch] = useReducer(reducer, 0)
+// dispatch({ type: , payload:  })
+export const reducer = function(state, action) {
+    // reducer 의 역할은 state를 효율적으로 관리하는 것.
+
+    console.log("Reducer", "State => ", state, "Action => ", action)
+    
+}
