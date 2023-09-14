c003e2cfeea559a7ac71d3d3da1d26fc25ad5e30
Min Chang Jang <mcjang1116@gmail.com>
2023-09-14 09:23:31 +0900

    useReducer

--- src/components/hooks/reducer/CalcReducer.js
+++ src/components/hooks/reducer/CalcReducer.js
@@ -1,8 +1,23 @@
 // const [money, dispatch] = useReducer(reducer, 0)
-// dispatch({ type: , payload:  })
+// dispatch({ type: "power", payload: 10  })
 export const reducer = function(state, action) {
     // reducer 의 역할은 state를 효율적으로 관리하는 것.
 
     console.log("Reducer", "State => ", state, "Action => ", action)
-    
+
+    if (action.type == "power") {
+        // reducer 에서 return을 하게 되면, state값이 갱신된다.
+        // state가 갱신되면 이 Reducer를 사용하는 컴포넌트가 갱신된다.
+        return action.payload * action.payload
+    }
+    else if (action.type == "mod") {
+        // reducer 에서 return을 하게 되면, state값이 갱신된다.
+        // state가 갱신되면 이 Reducer를 사용하는 컴포넌트가 갱신된다.
+        return action.payload % 2
+    }
+    else {
+        // type이 power, mod 가 아니라면 원래의 state값을 반환한다.
+        return state;
+    }
+
 }
