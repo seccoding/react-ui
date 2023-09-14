8d0f7c199094a7ea092bd8d9ed7ae2477b2d5cf3
Min Chang Jang <mcjang1116@gmail.com>
2023-09-14 14:36:04 +0900

    JsonPlaceholder

--- src/components/axios/JsonPlaceholder.js
+++ src/components/axios/JsonPlaceholder.js
@@ -1,17 +1,22 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function JsonPlaceholder() {
+
+    const [posts, setPosts] = useState([])
+    const [status, setStatus] = useState(0)
 
     // componentDidMount 해당.
     useEffect(() => {
         const readPostsUrl = "https://jsonplaceholder.typicode.com/posts"
         axios.get(readPostsUrl)
              .then((response) => {
-                console.log(response)
+                setPosts(response.data)
+                setStatus(response.status)
              }) // 성공 Case (요청이 성공했다. 응답도 정상적으로 도착했다.)
              .catch((error) => {
-                console.log(error)
+                setPosts([])
+                setStatus(error.response.status)
              }) // 실패 Case (요청이 실패했다. 응답이 제대로 오지 않았거나 응답에 에러 메시지가 포함되어 있다.)
     }, [])
 
