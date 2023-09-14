9bb2cad47c210068356924595c1b23d01f0cf871
Min Chang Jang <mcjang1116@gmail.com>
2023-09-14 09:32:26 +0900

    ReducerComponent

--- src/components/hooks/ReducerComponent.js
+++ src/components/hooks/ReducerComponent.js
@@ -11,8 +11,8 @@ export default function ReducerComponent() {
             <h1>계산기</h1>
             <h3>결과: {result}</h3>
             <input type="number" ref={numberRef} />
-            <button onClick={() => dispatch()}>제곱구하기</button>
-            <button onClick={() => dispatch()}>나머지구하기</button>
+            <button onClick={() => dispatch({ type: "power", payload: numberRef.current.value })}>제곱구하기</button>
+            <button onClick={() => dispatch({ type: "mod", payload: numberRef.current.value })}>나머지구하기</button>
         </>
     );
 
