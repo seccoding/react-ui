b1e7f8f20b69bbb774b5e105ad90d55afb616eb4
Min Chang Jang <mcjang1116@gmail.com>
2023-09-14 15:22:34 +0900

    JsonPlaceholder

--- src/components/axios/JsonPlaceholder.js
+++ src/components/axios/JsonPlaceholder.js
@@ -32,6 +32,30 @@ export default function JsonPlaceholder() {
              }) // 실패 Case (요청이 실패했다. 응답이 제대로 오지 않았거나 응답에 에러 메시지가 포함되어 있다.)
     }
 
+    function addNewPost() {
+        const readPostsUrl = "https://jsonplaceholder.typicode.com/posts"
+        axios.post(readPostsUrl, {
+                title: "등록합니다.",
+                body: "새로운 글이에요.",
+                userId: 1
+             })
+             .then((response) => {
+                // 새로 글 목록을 조회하는 기능.
+
+                setPosts( [...posts, {
+                    title: "등록합니다.",
+                    body: "새로운 글이에요.",
+                    userId: 1,
+                    id: 201
+                }] )
+                setStatus(response.status)
+             }) // 성공 Case (요청이 성공했다. 응답도 정상적으로 도착했다.)
+             .catch((error) => {
+                setPosts([...posts])
+                setStatus(error.response.status)
+             }) // 실패 Case (요청이 실패했다. 응답이 제대로 오지 않았거나 응답에 에러 메시지가 포함되어 있다.)
+    }
+
     // componentDidMount 해당.
     // useEffect(() => {
     //     const readPostsUrl = "https://jsonplaceholder.typicode.com/posts"
@@ -49,7 +73,9 @@ export default function JsonPlaceholder() {
     if (status == 404) {
         return (
             <div>
-                <Buttons getAllPosts={getAllPosts} getOnePost={getOnePost} /> 
+                <Buttons getAllPosts={getAllPosts} 
+                         getOnePost={getOnePost}
+                         addNewPost={addNewPost} /> 
                 URL이 올바르지 않습니다.
             </div>
         );
@@ -57,7 +83,9 @@ export default function JsonPlaceholder() {
     else {
         return (
             <div>
-                <Buttons getAllPosts={getAllPosts} getOnePost={getOnePost} /> 
+                <Buttons getAllPosts={getAllPosts} 
+                         getOnePost={getOnePost}
+                         addNewPost={addNewPost} /> 
                 {
                     posts.map(post => <div key={post.id}>{post.title}</div>)
                 }
@@ -66,13 +94,13 @@ export default function JsonPlaceholder() {
     }
 }
 
-function Buttons( {getAllPosts, getOnePost} ) {
+function Buttons( {getAllPosts, getOnePost, addNewPost} ) {
 
     return (
         <div>
             <button onClick={getAllPosts}>조회</button>
             <button onClick={getOnePost}>한건 조회</button>
-            <button>등록</button>
+            <button onClick={addNewPost}>등록</button>
             <button>수정</button>
             <button>삭제</button>
         </div>
