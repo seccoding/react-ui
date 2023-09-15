579b9c99f1f1914231488b9dad9f1194e6e149fc
Min Chang Jang <mcjang1116@gmail.com>
2023-09-15 10:46:42 +0900

    Router

--- src/MainApp.js
+++ src/MainApp.js
@@ -1,14 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import BoardList, { Detail, Write } from "./components/board/BoardList";
+import { useState } from "react";
 
 export default function MainApp() {
+
+    const [item, setItem] = useState([])
 
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={ <BoardList /> } />
-                <Route path="/write" element={ <Write /> } />
-                <Route path="/view/:num" element={ <Detail /> } />
+                <Route path="/" element={ <BoardList item={item} /> } />
+                <Route path="/write" element={ <Write item={item} setItem={setItem}/> } />
+                <Route path="/view/:num" element={ <Detail item={item} /> } />
             </Routes>
         </BrowserRouter>
     );
--- src/components/board/BoardList.js
+++ src/components/board/BoardList.js
@@ -1,8 +1,7 @@
-import { useState } from "react";
+import { useRef } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
-export default function BoardList() {
-
-    const [item, setItem] = useState([])
+export default function BoardList( {item} ) {
 
     return (
         <div>
@@ -25,6 +24,9 @@ export default function BoardList() {
                     }
                 </tbody>
             </table>
+            <div>
+                <Link to="/write">글쓰기</Link>
+            </div>
         </div>
     );
 }
@@ -42,10 +44,74 @@ function Row( {num, title, author, viewCount} ) {
 
 }
 
-export function Detail() {
+export function Detail( {item} ) {
+
+    // /view/:num 의 값을 받아옴.
+    const {num} = useParams()
+    const detailItem = item.filter((each) => each.num == num)[0]
+
+    return (
+        <div>
+            <div>
+                번호: {detailItem.num}
+            </div>
+            <div>
+                제목: {detailItem.title}
+            </div>
+            <div>
+                작성자: {detailItem.author}
+            </div>
+            <div>
+                조회수: {detailItem.viewCount}
+            </div>
+            <div>
+                내용: {detailItem.desc}
+            </div>
+            <div>
+                <Link to="/">목록으로 돌아가기</Link>
+            </div>
+        </div>
+    );
+
 
 }
 
-export function Write() {
+export function Write( {item, setItem} ) {
+
+    const titleRef = useRef()
+    const authorRef = useRef()
+    const descRef = useRef()
+
+    const navigate = useNavigate()
+
+    function save() {
+        setItem( [...item, {
+            num: item.length + 1,
+            title: titleRef.current.value,
+            author: authorRef.current.value,
+            desc: descRef.current.value,
+            viewCount: 0
+        }] )
+
+        navigate("/")
+    }
+
+    return (
+        <div>
+            <div>
+                <input type="text" placeholder="제목" ref={titleRef} />
+            </div>
+            <div>
+                <input type="text" placeholder="작성자" ref={authorRef} />
+            </div>
+            <div>
+                <textarea placeholder="내용" ref={descRef}></textarea>
+            </div>
+            <div>
+                <button onClick={save}>저장</button>
+                <Link to="/">목록으로 돌아가기</Link>
+            </div>
+        </div>
+    );
 
 }
