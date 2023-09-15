fbf971a2593aaa46df2a6b3f1993b5d822ef0737
Min Chang Jang <mcjang1116@gmail.com>
2023-09-15 12:44:01 +0900

    Router

--- src/MainApp.js
+++ src/MainApp.js
@@ -1,6 +1,10 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./MainApp.css"
+
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import BoardList, { Detail, Write } from "./components/board/BoardList";
 import { useState } from "react";
+import ComplexReducerHook from "./components/hooks/ComplexReducerHook";
+import HelloReact from "./components/hello/HelloReact";
 
 export default function MainApp() {
 
@@ -8,11 +12,25 @@ export default function MainApp() {
 
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={ <BoardList item={item} /> } />
-                <Route path="/write" element={ <Write item={item} setItem={setItem} /> } />
-                <Route path="/view/:num" element={ <Detail item={item} setItem={setItem} /> } />
-            </Routes>
+            <div id="grid">
+                <div id="header">여기는 Header입니다.</div>
+                <div id="aside">
+                    <ul>
+                        <li><Link to="/">게시판</Link></li>
+                        <li><Link to="/todo">Todo</Link></li>
+                        <li><Link to="/hello">HelloReact</Link></li>
+                    </ul>
+                </div>
+                <div>
+                    <Routes>
+                        <Route path="/" element={ <BoardList item={item} /> } />
+                        <Route path="/write" element={ <Write item={item} setItem={setItem} /> } />
+                        <Route path="/view/:num" element={ <Detail item={item} setItem={setItem} /> } />
+                        <Route path="/todo" element={ <ComplexReducerHook /> } />
+                        <Route path="/hello" element={ <HelloReact /> } />
+                    </Routes>
+                </div>
+            </div>
         </BrowserRouter>
     );
 }
